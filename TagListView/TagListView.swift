2858091f3b2a7c4bd102ce9de00d62f0b7ca19a8
Amon <amon@cupist.com>
2018-03-13 15:19:20 +0900

    Setting up property type to open: tagViewHeight

--- TagListView/TagListView.swift
+++ TagListView/TagListView.swift
@@ -202,7 +202,7 @@ open class TagListView: UIView {
     open private(set) var tagViews: [TagView] = []
     private(set) var tagBackgroundViews: [UIView] = []
     private(set) var rowViews: [UIView] = []
-    private(set) var tagViewHeight: CGFloat = 0
+    open private(set) var tagViewHeight: CGFloat = 0
     private(set) var rows = 0 {
         didSet {
             invalidateIntrinsicContentSize()
